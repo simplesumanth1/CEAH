89a94d9b3c112a0b4a355d5a3457f780a656750a
simplesumanth1 <105303952+simplesumanth1@users.noreply.github.com>
2024-12-13 13:36:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=`, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- font awesome -->
+    <script src="https://kit.fontawesome.com/f5b2fd4dd9.js" crossorigin="anonymous"></script>
+</head>
+
+<body >
+
+    <!-- nav bar -->
+    <nav class="navbar navbar-expand-sm navbar shadow-lg fixed-top ">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="javascript:void(0)">
+            
+            <span class="p-2 rounded-3 border" style="font-family: cursive; box-shadow: 1px 5px 2px blueviolet;" >CEAH</span></a>
+            <marquee behavior="" direction="">
+                <span class="p-2 rounded-3 border text-warning" style="font-family: cursive; box-shadow: 1px 5px 2px blueviolet;" >Campus Events and Activities Hub</span>
+            </marquee>
+            
+
+        </div>
+      </nav>
+
+
+      <div class="container " style="margin-top: 100px;">
+        <div class="card rounded-4 " style="
+        background: #E5E6DE;
+        background: radial-gradient(at center bottom, #E5E6DE, #EDEFA2);border: 5px solid blueviolet;">
+            <div class="card-header">
+                <div class="card-title text-center text-success h3">
+                    Who we are
+
+                </div>
+                <div class="card-body b">
+                    <p class="lead p">
+                        Welcome to Campus Events and Activities Hub! We are a dedicated platform designed to bring the
+                        vibrant and diverse campus community together through a variety of events and activities. Our
+                        mission is to enhance the overall campus experience by providing a central hub for organizing,
+                        discovering, and participating in a wide range of events.
+                    </p>
+                    <br>
+                    <span class="float-end text-secondary">--CEAH team</span>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+
+
+
+    <div class="container  " style="margin-top: 100px;">
+        <div class="row g-3 h-100">
+            <div class="col-md-3">
+                <div class="card shadow-lg  border-3 rounded-3 h-100" style="border-top-color:  blueviolet;
+                border-bottom-color:aquamarine">
+                    <div class="card-img-top">
+                        <div class="ratio ratio-4x3 card-img-fluid ">
+                            <img src="./stundentImage.jpg" alt="" srcset="">
+
+                        </div>
+
+                    </div>
+                    <div class="card-header ">
+                        <div class="card-title  badge bg-info p-2 text-center">
+                            Student Login
+
+                        </div>
+                        
+
+                    </div>
+                    <div class="card-body">
+                        <span class="lead">To know about Campus Activities and Events Login</span>
+
+                    </div>
+                    <div class="card-footer text-center">
+                        <a href="Student\studentLogin.php">
+                            <button class="btn shadow-lg rounded-3" style=" border: 4px solid white; border-top-color: blueviolet;border-bottom-color: aquamarine;">
+                                <b>Student Login&nbsp;<i class="fa-solid fa-right-to-bracket"></i></b>
+    
+                            </button>
+                        </a>
+
+                    </div>
+    
+                </div>
+
+            </div>
+
+            <!-- for club members -->
+            <div class="col-md-3">
+                <div class="card shadow-lg border-3  rounded-3 h-100" style="border-top-color:  blueviolet;
+                border-bottom-color:aquamarine">
+                    <div class="card-img-top">
+                        <div class="ratio ratio-4x3 card-img-fluid ">
+                            <img src="./clubLead.jpeg" alt="" srcset="">
+
+                        </div>
+
+                    </div>
+                    <div class="card-header ">
+                        <div class="card-title  badge bg-info p-2 text-center">
+                            Club Lead Login
+
+                        </div>
+                        
+
+                    </div>
+                    <div class="card-body">
+                        <span class="lead">To add or Schedule the Events</span>
+
+                    </div>
+                    <div class="card-footer text-center">
+                        <a href="ClubLead\clubLeadLogin.php">
+                            <button class="btn shadow-lg rounded-3" style=" border: 4px solid white; border-top-color: blueviolet;border-bottom-color: aquamarine;">
+                                <b>Club Lead Login&nbsp;<i class="fa-solid fa-right-to-bracket"></i></b>
+    
+                            </button>
+                        </a>
+
+                    </div>
+    
+                </div>
+
+            </div>
+            <!-- for admin -->
+            <!--  -->
+            <div class="col-md-3">
+                <div class="card h-100 shadow-lg  border-3 rounded-3 " style="border-top-color:  blueviolet;
+                border-bottom-color:aquamarine">
+                    <div class="card-img-top">
+                        <div class="ratio ratio-4x3 card-img-fluid ">
+                            <img src="./admin.jpeg" alt="" srcset="">
+
+                        </div>
+
+                    </div>
+                    <div class="card-header ">
+                        <div class="card-title  badge bg-info p-2 text-center">
+                            Admin Login
+
+                        </div>
+                        
+
+                    </div>
+                    <div class="card-body">
+                        <span class="lead">To Control Whole System</span>
+
+                    </div>
+                    <div class="card-footer text-center">
+                        <a href="Admin\adminLogin.php">
+                            <button class="btn shadow-lg rounded-3  " style=" border: 4px solid white; border-top-color: blueviolet;border-bottom-color: aquamarine;">
+                                <b >Admin Login&nbsp;<i class="fa-solid fa-right-to-bracket"></i></b>
+    
+                            </button>
+                        </a>
+
+                    </div>
+    
+                </div>
+
+            </div>
+
+
+            <!-- for judge login -->
+            <div class="col-md-3">
+                <div class="card h-100 shadow-lg  border-3 rounded-3 " style="border-top-color:  blueviolet;
+                border-bottom-color:aquamarine">
+                    <div class="card-img-top">
+                        <div class="ratio ratio-4x3 card-img-fluid ">
+                            <img src="./judge.png" alt="" srcset="">
+
+                        </div>
+
+                    </div>
+                    <div class="card-header ">
+                        <div class="card-title  badge bg-info p-2 text-center">
+                            Event Judge Login
+
+                        </div>
+                        
+
+                    </div>
+                    <div class="card-body">
+                        <span class="lead">To Judge the Performaces and decide the winners</span>
+
+                    </div>
+                    <div class="card-footer text-center">
+                        <a href="Judge\judgeLogin.php">
+                            <button class="btn shadow-lg rounded-3  " style=" border: 4px solid white; border-top-color: blueviolet;border-bottom-color: aquamarine;">
+                                <b >Judge Login&nbsp;<i class="fa-solid fa-right-to-bracket"></i></b>
+    
+                            </button>
+                        </a>
+
+                    </div>
+    
+                </div>
+
+            </div>
+
+
+            <div class="col-md-3">
+                <div class="card h-100 shadow-lg  border-3 rounded-3 "style="border-top-color:  blueviolet;
+                border-bottom-color:aquamarine">
+                    <div class="card-img-top">
+                        <div class="ratio ratio-4x3 card-img-fluid ">
+                            <img src="./cont.jpeg" alt="" srcset="">
+
+                        </div>
+
+                    </div>
+                    <div class="card-header ">
+                        <div class="card-title  badge bg-info p-2 text-center">
+                            Contestant Login
+
+                        </div>
+                        
+
+                    </div>
+                    <div class="card-body">
+                        <span class="lead">If you are you Contestant in a particular event check your results here</span>
+
+                    </div>
+                    <div class="card-footer text-center">
+                        <a href="Contestant\contestantLogin.php">
+                            <button class="btn shadow-lg rounded-3  " style=" border: 4px solid white; border-top-color: blueviolet;border-bottom-color: aquamarine;">
+                                <b >Contestant Login&nbsp;<i class="fa-solid fa-right-to-bracket"></i></b>
+    
+                            </button>
+                        </a>
+
+                    </div>
+    
+                </div>
+
+            </div>
+
+        </div>
+
+
+    </div>
+
+    <!-- footer -->
+    <div class="container-fluid mt-5 text-bg-dark">
+        <div class="row">
+            <div class="col-md-3">
+
+            </div>
+            <div class="col-md-6 text-center">
+                <span class="text-center text-warning">Designed and Devoloped by Sumanth_Narasimhulu , Sirisha, Lakshmi</span>
+
+            </div>
+            <div class="col-md-3">
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+    
+
+</body>
+
+</html>
